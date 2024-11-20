e0e784982caa28f118a13c7957596e64a523c81b
karinka <student@CLASS-50-07>
2024-11-20 13:26:37 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 from database import Database
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,19 +7,20 @@ def home():
     return render_template("home.jinja")
 
 @app.route("/employee")
-def employee():    
+def employee():
     db = Database()
-    result_raw = db.get_employee()
-    result = []
-    for r in result_raw:
-        result.append(str(r["id"]) + ": " + r["name"] + r["post"])
-    return render_template("employee.jinja") 
+    emp = db.get_employee()     
+    return render_template("employee.jinja", emp = emp)
+@app.route("/price")
+def price():    
+    return render_template("price.jinja") 
+@app.route("/registration")
+def registration():    
+    return render_template("registration.jinja")
     
-        
-
-
 def main():
     app.run("0.0.0.0", 8000,True)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
